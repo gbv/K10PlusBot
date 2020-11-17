cae009288c6162d35e7f73f9c76449dedc6c8bf4
Jakob Voss <voss@gbv.de>
2020-11-17 09:36:14 +0100

    Add script to remove statements on human items

---
+++ remove-human-claims.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# remove K10plus PPPN statements on humans, property should only be used on bibliographic records
+
+wd sparql -f table <(echo 'SELECT ?qid ?ppn { ?qid wdt:P31 wd:Q5 ; wdt:P6721 ?ppn }') | tail -n +2 | \
+while read QID PPN
+do
+    GUID=$(wd claims "$QID" P6721 "$PPN")
+    if [ ! -z "$GUID" ]; then
+        echo "-$QID $PPN"
+        wd remove-claim "$GUID"
+        sleep 10
+    fi
+done

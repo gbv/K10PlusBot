e500b372b72418b6c1cc232ef76d31d64ba3e931
Jakob Voß <voss@gbv.de>
2020-11-18 08:49:19 +0100

    Extend stats.sh

--- stats.sh
+++ stats.sh
@@ -2,8 +2,14 @@
 set -e
 
 count () {
-  wd sparql <(echo "SELECT (COUNT(?item) AS ?count) { ?item wdt:$1 ?value }")
+  wd sparql <(echo "SELECT (COUNT(?x) AS ?count) { $1 }")
 }
 
-# PPN,ISBN-10,ISBN-13
-echo -e "$(date '+%Y-%m-%d')\t$(count P6721)\t$(count P957)\t$(count P212)"
+P6721=$(count "?x wdt:P6721 []")
+ISBN10=$(count "?x wdt:P957 []")
+ISBN13=$(count "?x wdt:P212 []")
+ISBN=$(count "{ ?x wdt:P957 [] } UNION { ?x wdt:P212 [] }")
+NOKXP=$(count "{ ?x wdt:P957 [] } UNION { ?x wdt:P212 [] } FILTER NOT EXISTS { ?x wdt:P6721 [] }")
+CHECKED=$(wc -l < isbn-looked-up-in-kxp.txt)
+
+echo -e "$(date '+%Y-%m-%d')\t$P6721\t$ISBN10\t$ISBN13\t$ISBN\t$NOKXP\t$CHECKED"

be989bcef6bba9deed360ac89e293b50f4d97902
Jakob Voss <voss@gbv.de>
2020-12-01 12:44:08 +0100

    Avoid error in remove-human-claims.sh

--- remove-human-claims.sh
+++ remove-human-claims.sh
@@ -6,7 +6,7 @@ set -e
 wd sparql -f table <(echo 'SELECT ?qid ?ppn { ?qid wdt:P31 wd:Q5 ; wdt:P6721 ?ppn }') | tail -n +2 | \
 while read QID PPN
 do
-    GUID=$(wd claims "$QID" P6721 "$PPN")
+    GUID=$(wd claims "$QID" P6721 "$PPN" || true 2> /dev/null)
     if [ ! -z "$GUID" ]; then
         echo "-$QID $PPN"
         wd remove-claim "$GUID"
